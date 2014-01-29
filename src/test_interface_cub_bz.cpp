c3c8f4b78d98a20cac8523a6b5253d7cd815f9fc
Viktor Kopp <vifactor@gmail.com>
2014-01-29 17:11:43 +0100

    New unit tests are added to ckeck the case z > d (observation point lays below the interface)

--- src/test_interface_cub_bz.cpp
+++ src/test_interface_cub_bz.cpp
@@ -28,6 +28,30 @@ struct FixtureInterfaceCubBz
 		double d = 12;
 		double z = 2;
 
+		/**values obtained in the Mathematica*/
+		Wxxxx_bx = 0.0;
+		Wzzzz_bx = 0.0;
+		Wxzxz_bx = 0.0;
+		Wxxzz_bx = 0;
+		Wzxzx_bx = 0;
+		Wxzzx_bx = 0;
+
+		Wxxxx_bz = 0.00299662;
+		Wzzzz_bz = 0.000867747;
+		Wxzxz_bz = 0.0241757;
+		Wxxzz_bz = -0.0016064;
+		Wzxzx_bz = 0.0355546;
+		Wxzzx_bz = -0.0283989;
+
+		Wyxyx_by = 0;
+		Wyzyz_by = 0;
+		wxx = 0.717299;
+		wyy = 0.706512;
+		wzz = 0.413883;
+		wxy = 0.00905059;
+		wxz = -0.580902;
+		wyz = -0.871353;
+
 		interface = new AnalyticalMisfitInterfaceCub(rho, bx, by, bz, Qx, Qy,
 				Qz, phi, nu, d);
 		interface->init(z);
@@ -40,6 +64,11 @@ struct FixtureInterfaceCubBz
         	delete interface;
         interface = NULL;
     }
+    double Wxxxx_bx, Wzzzz_bx, Wxzxz_bx, Wxxzz_bx, Wzxzx_bx, Wxzzx_bx;
+    double Wxxxx_bz, Wzzzz_bz, Wxzxz_bz, Wxxzz_bz, Wzxzx_bz, Wxzzx_bz;
+    double Wyxyx_by, Wyzyz_by;
+    double wxx, wyy, wzz, wxy, wxz, wyz;
+
     AnalyticalMisfitInterfaceCub * interface;
 };
 
@@ -47,122 +76,102 @@ BOOST_FIXTURE_TEST_SUITE(meanDistorsions, FixtureInterfaceCubBz)
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxxxx_bx)
 {
-	double val = 0.00622437;
-	BOOST_CHECK_CLOSE(interface->Wxxxx_bx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxxxx_bx(), Wxxxx_bx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wzzzz_bx)
 {
-	double val = 0.00158983;
-	BOOST_CHECK_CLOSE(interface->Wzzzz_bx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wzzzz_bx(), Wzzzz_bx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxzxz_bx)
 {
-	double val = 0.0056553;
-	BOOST_CHECK_CLOSE(interface->Wxzxz_bx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxzxz_bx(), Wxzxz_bx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxxzz_bx)
 {
-	double val = -0.00314146;
-	BOOST_CHECK_CLOSE(interface->Wxxzz_bx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxxzz_bx(), Wxxzz_bx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wzxzx_bx)
 {
-	double val = 0.00701169;
-	BOOST_CHECK_CLOSE(interface->Wzxzx_bx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wzxzx_bx(), Wzxzx_bx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxzzx_bx)
 {
-	double val = -0.00591633;
-	BOOST_CHECK_CLOSE(interface->Wxzzx_bx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxzzx_bx(), Wxzzx_bx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxxxx_bz)
 {
-	double val = 0.00299662;
-	BOOST_CHECK_CLOSE(interface->Wxxxx_bz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxxxx_bz(), Wxxxx_bz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wzzzz_bz)
 {
-	double val = 0.000867747;
-	BOOST_CHECK_CLOSE(interface->Wzzzz_bz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wzzzz_bz(), Wzzzz_bz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxzxz_bz)
 {
-	double val = 0.0241759;
-	BOOST_CHECK_CLOSE(interface->Wxzxz_bz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxzxz_bz(), Wxzxz_bz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxxzz_bz)
 {
-	double val = -0.0016064;
-	BOOST_CHECK_CLOSE(interface->Wxxzz_bz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxxzz_bz(), Wxxzz_bz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wzxzx_bz)
 {
-	double val = 0.0355543;
-	BOOST_CHECK_CLOSE(interface->Wzxzx_bz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wzxzx_bz(), Wzxzx_bz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wxzzx_bz)
 {
-	double val = -0.0283989;
-	BOOST_CHECK_CLOSE(interface->Wxzzx_bz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wxzzx_bz(), Wxzzx_bz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wyxyx_by)
 {
-	double val = 0.0134524;
-	BOOST_CHECK_CLOSE(interface->Wyxyx_by(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wyxyx_by(), Wyxyx_by, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_Wyzyz_by)
 {
-	double val = 0.00018947;
-	BOOST_CHECK_CLOSE(interface->Wyzyz_by(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->Wyzyz_by(), Wyzyz_by, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_wxx)
 {
-	double val = 0.195903;
-	BOOST_CHECK_CLOSE(interface->wxx(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->wxx(), wxx, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_wzz)
 {
-	double val = 0.150516;
-	BOOST_CHECK_CLOSE(interface->wzz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->wzz(), wzz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_wxz)
 {
-	double val = -0.175359;
-	BOOST_CHECK_CLOSE(interface->wxz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->wxz(), wxz, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_wyy)
 {
-	double val = 0.173499;
-	BOOST_CHECK_CLOSE(interface->wyy(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->wyy(), wyy, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_wxy)
 {
-	double val = 0.0187993;
-	BOOST_CHECK_CLOSE(interface->wxy(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->wxy(), wxy, 1e-2);
 }
 
 BOOST_AUTO_TEST_CASE(Burgers_bz_component_wyz)
 {
-	double val = -0.263038;
-	BOOST_CHECK_CLOSE(interface->wyz(), val, 1e-2);
+	BOOST_CHECK_CLOSE(interface->wyz(), wyz, 1e-2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
